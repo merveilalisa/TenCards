654701a7b9dec69b5aed068dc65f8ca97e4e7076
merveilalisa <merveilalisa@gmail.com>
2017-09-25 23:23:54 -0400

    ten cards
    with card counter in the header

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,100 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  cards = [
+
+    {
+      imgPath: "one.jpg",
+      title: "Dessert of Delight",
+      desc:"Oh my should you try this!",
+      favorited:false
+    },
+
+    {
+      imgPath: "two.jpg",
+      title: "Wonderful Suprise",
+      desc:"Something to Make you melt.",
+      favorited:false
+    },
+
+
+    {
+      imgPath: "three.jpg",
+      title: "Strawberry Suprise",
+      desc:"Berry of all berries.",
+      favorited:false
+    },
+    {
+      imgPath: "four.jpg",
+      title: "Sweet Goodness",
+      desc:"This is a wonderful treat.",
+      favorited:false
+    },
+
+    {
+      imgPath: "five.jpg",
+      title: "Sweet Tooth",
+      desc:"back to the Dentist with this one!",
+      favorited:false
+    },
+
+    {
+      imgPath: "six.jpg",
+      title: "Macaroons",
+      desc:"This is the great one",
+      favorited:false
+    },
+    {
+      imgPath: "seven.jpg",
+      title: "Ice Cream",
+      desc:"This is a wonderful gift for your mom",
+      favorited:false
+    },
+
+
+    {
+      imgPath: "eight.jpg",
+      title: "Fruit",
+      desc:"Healthier Alternative!",
+      favorited:false
+    },
+    {
+      imgPath: "nine.jpg",
+      title: "Chocolate",
+      desc:"For all the chocolate lovers out there",
+      favorited:false
+    },
+
+    {
+      imgPath: "ten.jpg",
+      title: "Wonderful",
+      desc:"This is a wonderful dessert",
+      favorited:false
+    }
+
+
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  clickfav(card){
+    let index =this.cards.indexOf(card);
+    if(index > -1){
+      if(card.favorited){
+        this.cards[index].favorited = false;
+      }else{
+        this.cards[index].favorited = true;
+
+      }
+    }
+  }
+
+
+
+
+
+
 
 }
